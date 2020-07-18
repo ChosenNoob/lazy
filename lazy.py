681d5567c5bd4653c69d376bd2376d729e5d9fb7
Baran Karaaslan <barankaraaslan_06@hotmail.com>
2020-07-18 22:41:49 +0300

    Release 0.1.0
    Minimal implementation
    Python client and server is implemented
    Android client will be implemented

--- lazy.py
+++ lazy.py
@@ -1,5 +1,6 @@
 #!/bin/python
 
+import subprocess
 from socket import socket, AF_INET, SOCK_STREAM, gethostname
 from pypret.interpreter import Interpreter
 from sys import argv
@@ -22,10 +23,11 @@ def connect_to_host(hostname):
 	# returns socket if succeeds 
 	# returns None if fails
 	sock = socket(AF_INET, SOCK_STREAM)
-	conn_result = sock.connect_ex((hostname, PORT))
-	if conn_result == 0:
+	print("Connecting {}".format(hostname))
+	try:
+		conn_result = sock.connect((hostname, PORT))
 		return sock
-	else:
+	except Exception:
 		sock.close()
 		return None
 
@@ -50,11 +52,17 @@ def connect_existing_hosts():
 	if isfile('hostnames.txt'):
 		with open("hostnames.txt") as hostname_file:
 			hostnames = hostname_file.readlines()
+			print(hostnames)
 			for hostname in hostnames:
-				return connect_to_host(hostname)
-	
+				return connect_to_host(hostname.strip())
+
 def conn_handling(sock):
 	print("Connected")
+	while True:
+		request = input("<<<")
+		sock.sendall(request.encode())
+		response = sock.recv(1024).decode()
+		print(response)
 	sock.close()
 
 def run_as_client():
@@ -67,12 +75,38 @@ def run_as_client():
 			conn_handling(conn_result)
 
 # Server mode
+def load_commands():
+	# loads known commands from commands.txt to server
+	commands = {}
+	if isfile("commands.txt"):
+		with open("commands.txt") as command_file:
+			lines = command_file.readlines()
+			for line in lines:
+				split_line = line.split(" ", 1)
+				commands[split_line[0]] = split_line[1].strip()
+	print(commands)
+	return commands
+
 def server_worker(client_conn, client_addr):
 	# Thread that handles each client connection
 	print('Handling ' + str(client_addr))
+	commands = load_commands()
 	while 1:
 		data = client_conn.recv(1024)
-		if not data: break
+		if not data: 
+			print("Connection closed by {}".format(client_addr))
+			break
+		request = data.decode()
+		print("{} requested {}".format(client_addr, request))
+		try:
+			subprocess.run(commands[request])
+			client_conn.sendall("success".encode())
+		except KeyError as e:
+			print(e)
+			client_conn.sendall("not a command".encode())
+		except Exception as e:
+			print(e)
+			client_conn.sendall("runtime error".encode())
 
 	client_conn.close()
 
